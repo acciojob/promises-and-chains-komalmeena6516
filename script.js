1508a64a6db38b018148e06067c6e0a3abe987c8
Komal Meena <80938016+komalmeena6516@users.noreply.github.com>
2023-03-15 22:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+
+            btn.addEventListener('click', run);
+
+            // console.log(name, age)
+
+            function checkAge() {
+                const umar = document.getElementById('age').value;
+                const naam = document.getElementById('name').value;
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        if (umar >= 18) {
+                            resolve("Welcome "+ naam +" You can vote")
+                        }
+                        else {
+                            reject("Sorry "+ naam +" You can not vote");
+                        }
+                    }, 4000);
+
+                })
+
+
+            }
+
+           function run(){
+                 checkAge().then((res) => {
+                alert(res);
+            })
+                .catch((err) => {
+                    alert(err);
+                });
+            }
